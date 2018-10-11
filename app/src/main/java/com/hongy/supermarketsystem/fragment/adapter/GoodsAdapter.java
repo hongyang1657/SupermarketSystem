a0e20b2f1f6f37df22e2078d77b3e5b252f5371a
hongy <gqgz20041231@qq.com>
2018-10-11 17:14:53 +0800

    加入recyclerview

---
+++ app/src/main/java/com/hongy/supermarketsystem/fragment/adapter/GoodsAdapter.java
@@ -0,0 +1,56 @@
+package com.hongy.supermarketsystem.fragment.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.hongy.supermarketsystem.R;
+import com.hongy.supermarketsystem.bean.Goods;
+
+import java.util.List;
+
+public class GoodsAdapter extends RecyclerView.Adapter<GoodsAdapter.MyViewHolder>{
+
+    private List<Goods> goodsList;
+    private Context mContext;
+
+    public GoodsAdapter(List<Goods> goodsList) {
+        this.goodsList = goodsList;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        mContext = viewGroup.getContext();
+        View view = LayoutInflater.from(mContext).inflate(R.layout.goods_item,viewGroup,false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
+        myViewHolder.tvName.setText(goodsList.get(i).getName());
+        myViewHolder.tvPrice.setText(goodsList.get(i).getPrice());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return goodsList.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+        private ImageView imageView;
+        private TextView tvName;
+        private TextView tvPrice;
+        private TextView tvBarcode;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
