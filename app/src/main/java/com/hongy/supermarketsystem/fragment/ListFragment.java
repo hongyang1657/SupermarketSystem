edd22d01fafed758f00ab1f303c34f21bb2bc69d
gqgz2 <gqgz20041231@qq.com>
2018-11-01 22:57:36 +0800

    加入搜索页面；添加了上拉加载新数据。

--- app/src/main/java/com/hongy/supermarketsystem/fragment/ListFragment.java
+++ app/src/main/java/com/hongy/supermarketsystem/fragment/ListFragment.java
@@ -2,7 +2,9 @@ package com.hongy.supermarketsystem.fragment;
 
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,12 +12,13 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.hongy.supermarketsystem.R;
 import com.hongy.supermarketsystem.bean.Goods;
 import com.hongy.supermarketsystem.fragment.adapter.GoodsAdapter;
-import com.hongy.supermarketsystem.utils.DataBaseUtil;
 import com.hongy.supermarketsystem.utils.L;
+import com.hongy.supermarketsystem.view.SearchActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,12 +27,15 @@ import cn.bmob.v3.BmobQuery;
 import cn.bmob.v3.exception.BmobException;
 import cn.bmob.v3.listener.FindListener;
 
+import static android.app.Activity.RESULT_OK;
+
 public class ListFragment extends Fragment implements GoodsAdapter.IitemSelect{
 
     private List<Goods> goodsList = new ArrayList<>();
     private GoodsAdapter adapter;
     private SwipeRefreshLayout swipeRefreshLayout;
     private RecyclerView recyclerView;
+    private TextView tvSearch;
 
     @Nullable
     @Override
@@ -48,9 +54,30 @@ public class ListFragment extends Fragment implements GoodsAdapter.IitemSelect{
     private void initView(View view){
         recyclerView = view.findViewById(R.id.recycler_view_search_list);
         swipeRefreshLayout = view.findViewById(R.id.list_swipe_layout);
+        tvSearch = view.findViewById(R.id.tv_search_goods);
         adapter = new GoodsAdapter(goodsList,this);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         recyclerView.setAdapter(adapter);
+        recyclerView.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                //L.i("onScrolled:  dx:"+dx+"   dy:"+dy);
+                if(!recyclerView.canScrollVertically(1)){
+                    L.i("滑动到底部"+goodsList.size());//滑动到底部
+                    //TODO 加载新数据
+                    loadMoreData(goodsList.size());
+                }
+                if(!recyclerView.canScrollVertically(-1)){
+                    L.i( "滑动到顶部");//滑动到顶部
+                }
+            }
+        });
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
@@ -58,15 +85,22 @@ public class ListFragment extends Fragment implements GoodsAdapter.IitemSelect{
                 refreshData();
             }
         });
-
+        tvSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //跳转搜索页面
+                Intent intent = new Intent(getActivity(), SearchActivity.class);
+                startActivityForResult(intent,1);
+            }
+        });
     }
 
+    //刷新数据
     private void refreshData(){
         //goodsList = DataBaseUtil.queryAll();      //本地数据库查询所有商品数据
 //        for (int i=0;i<goodsList.size();i++){
 //            L.i("goodsList:"+goodsList.get(i).toString());
 //        }
-
         //查询bomb数据库
         BmobQuery<Goods> query = new BmobQuery<>();
         query.setLimit(20);
@@ -76,7 +110,7 @@ public class ListFragment extends Fragment implements GoodsAdapter.IitemSelect{
                 if (e==null){
                     goodsList = list;
                     for (int i=0;i<goodsList.size();i++){
-                        L.i("查询bmob goodsList:"+goodsList.get(i).toString());
+                        //L.i("查询bmob goodsList:"+goodsList.get(i).toString());
                     }
                     adapter.notifyData(goodsList);
                 }else {
@@ -85,21 +119,52 @@ public class ListFragment extends Fragment implements GoodsAdapter.IitemSelect{
                 swipeRefreshLayout.setRefreshing(false);   //结束下拉刷新的动画
             }
         });
+    }
 
-
-
+    //加载更多数据
+    private void loadMoreData(int num){
+        BmobQuery<Goods> query = new BmobQuery<>();
+        query.setSkip(num);
+        query.setLimit(20);
+        query.findObjects(new FindListener<Goods>() {
+            @Override
+            public void done(List<Goods> list, BmobException e) {
+                if (e==null){
+                    goodsList.addAll(list);
+                    for (int i=0;i<goodsList.size();i++){
+                        //L.i("查询bmob goodsList:"+goodsList.get(i).toString());
+                    }
+                    adapter.notifyData(goodsList);
+                }else {
+                    L.i("查询bmob失败："+e.getMessage());
+                }
+            }
+        });
     }
 
     @Override
     public void onGoodsListChanged(List<Goods> goodsList) {
         for (int i=0;i<goodsList.size();i++){
-
-            L.i("goodsList:"+goodsList.get(i).toString());
+            //L.i("goodsList:"+goodsList.get(i).toString());
         }
     }
 
     @Override
     public void onItemLongClickListener(int position) {
         L.i("onItemLongClickListener:"+position);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case 1:
+                if (resultCode==RESULT_OK){
+                    String serachContent = data.getExtras().getString("serachContent");
+                    L.i("serachContent:"+serachContent);
+                    tvSearch.setText(serachContent);
+                }
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/hongy/supermarketsystem/view/SearchActivity.java
@@ -0,0 +1,44 @@
+package com.hongy.supermarketsystem.view;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.hongy.supermarketsystem.R;
+
+public class SearchActivity extends Activity{
+
+    private EditText etSerach;
+    private TextView tvSerach;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_serach);
+        initView();
+    }
+
+    private void initView(){
+        etSerach = findViewById(R.id.et_search);
+        tvSerach = findViewById(R.id.tv_bt_search);
+        etSerach.setFocusable(true);
+        etSerach.setFocusableInTouchMode(true);
+        etSerach.requestFocus();
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        tvSerach.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent();
+                intent.putExtra("serachContent",etSerach.getText().toString().trim());
+                setResult(RESULT_OK,intent);
+                finish();
+            }
+        });
+    }
+}
